e3458733f705c9b22c1e728efee985b676318837
PNguyen2110 <nphuocnguyen997@gmail.com>
2022-08-04 15:16:30 +0700

    bài tập sass

---
+++ js/main.js
@@ -0,0 +1,33 @@
+function scrollHeader(){
+
+    window.onscroll = function scroll(){
+        
+        
+        if(document.body.scrollTop > 200 || document. documentElement.scrollTop > 200 ){
+           
+           var header = document.querySelector('#header')
+           header.setAttribute('class', 'scrollbar');
+            
+        }
+        else {
+            document.querySelector('#header').removeAttribute('class','scrollbar')
+        }
+    }
+}
+scrollHeader()
+
+// click show navigation
+
+document.querySelector('.bar a').onclick = function() {
+    
+    var barClick = document.querySelector('.bar').classList.toggle('barClick');
+  
+    if(barClick){
+       
+        document.body.style.overflow = 'hidden';
+    }
+    else{
+        document.body.style.overflow = 'visible';
+    }
+}
+
